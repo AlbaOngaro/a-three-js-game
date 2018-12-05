ddae8f3cee21076f96a7114bf29d3a079c697a7f
Alba Ongaro <alba_ongaro@hotmail.com>
2018-12-05 23:57:23 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const sass = require('node-sass');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/js/game.js',
+    output: {
+        filename: 'js/game.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'],{exclude:  ['index.html'],}),
+        new CopyWebpackPlugin([
+            { 
+                from: './src/scss/styles.scss', 
+                to: './css/styles.css', 
+                toType: 'file',
+                transform (content, path) {
+                    var result = sass.renderSync({
+                      data: content.toString('utf-8')
+                    });
+                
+                    return result.css.toString('utf-8');
+                }
+            },
+        ])
+    ]
+};
