c638a7462658f5751b3ecc9dc4ae1476661e3d43
Alba Ongaro <albaongaro@MacBook-Pro-di-Alba.local>
2018-12-19 22:48:01 +0100

    order webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const sass = require('node-sass');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
